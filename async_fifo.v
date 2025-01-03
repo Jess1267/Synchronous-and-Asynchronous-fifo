f0351b711b1491cea1b15858a82f9eb4df7846ce
Jyotika K <97504610+Jess1267@users.noreply.github.com>
2025-01-03 11:06:34 +0530

    Update async_fifo.v

--- async_fifo.v
+++ async_fifo.v
@@ -19,116 +19,77 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module asyn_fifo(full,empty,rd,
+                                 wrt,clk_wrt,clk_rd,reset,rd_en,wrt_en);
+input [3:0]wrt;
+input clk_wrt,clk_rd,reset,rd_en,wrt_en;
 
-module async_fifo(write_clk,read_clk,reset,write_data,read_data,read_enable,write_enable,valid,full,empty,overflow,underflow);
-parameter data_width = 6, depth = 16;
-// as for the depth of 16  we need 4 address bus width but for asynchrnous we need width of 1 extra
-parameter address_width = 4;
-input write_clk,read_clk,reset,read_enable,write_enable;
-output empty,full;
-output reg overflow,underflow,valid;
-input [data_width-1 : 0] write_data;
-output reg [data_width-1 : 0] read_data;
+output reg [3:0]rd;
+output full,empty;
 
-reg [address_width : 0] write_ptr, read_ptr;
+//
+reg [3:0] memory[7:0];
+reg [3:0] rd_ptr,wrt_ptr;// 1 extra bit 3 for memory location
 
-reg [data_width-1 : 0] fifo_mem[depth-1 : 0];
-
-reg [data_width-1 : 0] write_ptr_gray_s1,write_ptr_gray_s2,read_ptr_gray_s1,read_ptr_gray_s2;
-
-// writing data into fifo
-always @(posedge write_clk)
+//write
+always@(posedge clk_wrt)
 begin
-    if(reset)
-    begin
-        write_ptr <= 0;
+    if(reset)begin
+        wrt_ptr<=0;
     end
-    else 
-    begin
-        if(write_enable && !full)
-        begin
-            fifo_mem[write_ptr] <= write_data;
-            write_ptr <= write_ptr + 1;
-        end
+    else if(wrt_en && !full)begin
+        memory[wrt_ptr[2:0]]<=wrt;
+        wrt_ptr<=wrt_ptr+1;
     end
-end 
-
-// reading data from fifo
-always @(posedge read_clk)
-begin
-    if(reset)
-    begin
-        read_ptr <= 0;
-    end
-    else 
-    begin
-        if(read_enable && !empty)
-        begin
-            read_data <= fifo_mem[read_ptr];
-            read_ptr <= read_ptr + 1;
-        end
-    end
-end 
-
-// to determine the empty and full we need to convert the wrt_ptr and read_ptr into the gray code;
-// doing xor with the original and right_shifted of it
-
-wire [address_width : 0] read_ptr_gray , write_ptr_gray;
-assign read_ptr_gray = read_ptr ^ (read_ptr >> 1);
-assign write_ptr_gray = write_ptr ^ (write_ptr >> 1);
-
-
-
-// 2 stage synchronizer for wr_pointer wrt to read_clk;
-always @(posedge read_clk)
-begin
-    if(reset)
-    begin
-        write_ptr_gray_s1 <= 0;
-        write_ptr_gray_s2 <= 0;
-    end
-    else
-    begin
-        write_ptr_gray_s1 <= write_ptr_gray;
-        write_ptr_gray_s2 <= write_ptr_gray_s1;
-    end
-
 end
 
-
-// 2 stage synchronizer for read_pointer wrt to write_clk;
-always @(posedge write_clk)
+//read
+always@(posedge clk_rd)
 begin
-    if(reset)
-    begin
-        read_ptr_gray_s1 <= 0;
-        read_ptr_gray_s2 <= 0;
+    if(reset)begin
+        rd_ptr<=0;
     end
-    else
-    begin
-        read_ptr_gray_s1 <= read_ptr_gray;
-        read_ptr_gray_s2 <= read_ptr_gray_s1;
+    else if(rd_en && !empty)begin
+        rd<=memory[rd_ptr[2:0]];
+        rd_ptr<=rd_ptr+1;
     end
-
 end
 
-// empty and full condition
-// here we are checking for the gray right. 
-// so we need to check first two msb bits of read and write ptr because in binary to gray code the effect of msb will be in first two msb positions of gray code
+//full empty => sync.=> CDC 2 fifo sync. => grey code.
 
-assign empty = (write_ptr_gray_s2 == read_ptr_gray);
-assign full = (write_ptr_gray[address_width-1]!= read_ptr_gray_s2[address_width-1]) && 
-              (write_ptr_gray[address_width-2]!= read_ptr_gray_s2[address_width-2]) &&
-              (write_ptr_gray[address_width-3:0] == read_ptr_gray_s2[address_width-3 : 0]);
-              
-// full and empty conditions
-always @(posedge write_clk) overflow <= write_enable && full; 
-always @(posedge read_clk) 
+//Binary to grey
+wire [3:0] b_g_rd,b_g_wrt;
+assign b_g_rd=(rd_ptr^(rd_ptr>>1));
+assign b_g_wrt=(wrt_ptr^(wrt_ptr>>1));
+
+//Synchronization
+reg [3:0]wrt_sync1,wrt_sync2,rd_sync1,rd_sync2;
+
+always@(posedge clk_rd)
 begin
-    overflow <= read_enable && empty;
-    valid <= (read_enable && (!empty));
+    if(reset)begin
+        wrt_sync1<=0;
+        wrt_sync2<=0;
+    end
+    else begin
+        wrt_sync1<=b_g_wrt;
+        wrt_sync2<=wrt_sync1;
+    end
 end
 
+always@(posedge clk_wrt)
+begin
+    if(reset)begin
+        rd_sync1<=0;
+        rd_sync2<=0;
+    end
+    else begin
+        rd_sync1<=b_g_rd;
+        rd_sync2<=rd_sync1;
+    end
+end
 
-
+//full empty
+assign full=(rd_sync2[3]!=wrt_sync2[3] && rd_sync2[2]!=wrt_sync2[2] && rd_sync2[1:0]==wrt_sync2[1:0]);
+assign empty=(rd_sync2==wrt_sync2);
 endmodule
